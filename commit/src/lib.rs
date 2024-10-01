5185c6c6ea11a7335acbbfea9392fea6bbf068d0
fabri236 <fabrizio_m@hotmail.com>
2024-10-01 20:21:09 -0300

    commitment trait

---
+++ commit/src/lib.rs
@@ -0,0 +1,20 @@
+use ark_ff::Field;
+use std::ops::{Add, Mul};
+use sumcheck::polynomials::MultiPoint;
+
+trait CommmitmentScheme<F: Field> {
+    type Commitment: for<'a> Add<&'a Self::Commitment, Output = Self::Commitment>
+        + Mul<F, Output = Self::Commitment>;
+    type OpenProof;
+
+    fn new(vars: usize) -> Self;
+    fn commit_mle(&self, evals: &[F]) -> Self::Commitment;
+    fn open(
+        &self,
+        evals: &[F],
+        commitment: Self::Commitment,
+        point: &MultiPoint<F>,
+        eval: F,
+    ) -> Self::OpenProof;
+    fn verify(&self, commitment: Self::Commitment, point: &MultiPoint<F>, eval: F) -> bool;
+}
