c900a8b34a189138e38ec4cf14e825441b35bc76
fabri236 <fabrizio_m@hotmail.com>
2024-12-27 20:26:07 -0300

    init

--- automata/src/lib.rs
+++ automata/src/lib.rs
@@ -2,11 +2,14 @@
 
 // A transducer, a Mealy  machine
 pub trait FiniteAutomata {
+    /// type for initiallization, may be same as state
+    type Init;
     type State;
     /// Use () for machines without inputs
     type Input;
     /// Leave as () to have just an fsm, no transducer
     type Output;
+    fn init(init: Self::Init) -> Self;
     /// integrated transition and output function, mutating state
     /// ignore input when computing output for Moore machine.
     fn transition_mut(&mut self, input: Self::Input) -> Self::Output;
