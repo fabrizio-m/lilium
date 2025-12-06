00445fe50cd971e57209de95db8ea5cb92a391df
fabri236 <fabrizio_m@hotmail.com>
2025-12-05 21:25:26 -0300

    add some standard gates

---
+++ ccs/src/gates.rs
@@ -0,0 +1,179 @@
+use crate::constraint_system::{Constraints, Gate, Val};
+
+pub enum AddN<const IO: usize, const I: usize> {}
+
+impl<const IO: usize, const I: usize> Gate<IO, I, 1> for AddN<IO, I> {
+    fn gate<V: Val>(i: [V; I]) -> [V; 1] {
+        debug_assert_eq!(IO, I + 1);
+        let mut sum = i[0].clone();
+        for i in i.into_iter().skip(1) {
+            sum = sum + i;
+        }
+        [sum]
+    }
+
+    fn check<V: Val>(i: [V; I], o: [V; 1]) -> Constraints<V> {
+        debug_assert_eq!(IO, I + 1);
+        let [out] = o;
+        let mut sum = i[0].clone();
+        for i in i.into_iter().skip(1) {
+            sum = sum + i;
+        }
+        Constraints::from(sum - out)
+    }
+}
+
+pub enum SubN<const IO: usize, const I: usize> {}
+
+impl<const IO: usize, const I: usize> Gate<IO, I, 1> for SubN<IO, I> {
+    fn gate<V: Val>(i: [V; I]) -> [V; 1] {
+        debug_assert_eq!(IO, I + 1);
+        let mut sub = i[0].clone();
+        for i in i.into_iter().skip(1) {
+            sub = sub - i;
+        }
+        [sub]
+    }
+
+    fn check<V: Val>(i: [V; I], o: [V; 1]) -> Constraints<V> {
+        debug_assert_eq!(IO, I + 1);
+        let [out] = o;
+        let mut sub = i[0].clone();
+        for i in i.into_iter().skip(1) {
+            sub = sub - i;
+        }
+        Constraints::from(sub - out)
+    }
+}
+
+pub enum MulN<const IO: usize, const I: usize> {}
+
+impl<const IO: usize, const I: usize> Gate<IO, I, 1> for MulN<IO, I> {
+    fn gate<V: Val>(i: [V; I]) -> [V; 1] {
+        debug_assert_eq!(IO, I + 1);
+        let mut product = i[0].clone();
+        for i in i.into_iter().skip(1) {
+            product = product * i;
+        }
+        [product]
+    }
+
+    fn check<V: Val>(i: [V; I], o: [V; 1]) -> Constraints<V> {
+        debug_assert_eq!(IO, I + 1);
+        let [out] = o;
+        let mut product = i[0].clone();
+        for i in i.into_iter().skip(1) {
+            product = product + i;
+        }
+        Constraints::from(product - out)
+    }
+}
+
+pub type Add2 = AddN<3, 2>;
+pub type Sub2 = SubN<3, 2>;
+pub type Mul2 = MulN<3, 2>;
+
+pub enum Equality {}
+
+impl Gate<2, 2, 0> for Equality {
+    fn gate<V: Val>(_i: [V; 2]) -> [V; 0] {
+        []
+    }
+
+    fn check<V: Val>(i: [V; 2], _o: [V; 0]) -> Constraints<V> {
+        let [a, b] = i;
+        Constraints::from(a - b)
+    }
+}
+
+pub enum Zero {}
+
+impl Gate<1, 1, 0> for Zero {
+    fn gate<V: Val>(_i: [V; 1]) -> [V; 0] {
+        []
+    }
+
+    fn check<V: Val>(i: [V; 1], _o: [V; 0]) -> Constraints<V> {
+        let [x] = i;
+        Constraints::from(x)
+    }
+}
+
+pub enum Double {}
+
+impl Gate<2, 1, 1> for Double {
+    fn gate<V: Val>(i: [V; 1]) -> [V; 1] {
+        let [x] = i;
+        [x.clone() + x]
+    }
+
+    fn check<V: Val>(i: [V; 1], o: [V; 1]) -> Constraints<V> {
+        let ([a], [a2]) = (i, o);
+        Constraints::from(a - a2)
+    }
+}
+
+pub enum Square {}
+
+impl Gate<2, 1, 1> for Square {
+    fn gate<V: Val>(i: [V; 1]) -> [V; 1] {
+        let [x] = i;
+        [x.clone() * x]
+    }
+
+    fn check<V: Val>(i: [V; 1], o: [V; 1]) -> Constraints<V> {
+        let ([a], [aa]) = (i, o);
+        Constraints::from(a - aa)
+    }
+}
+
+pub enum Pow<const EXP: u8> {}
+
+impl<const EXP: u8> Gate<2, 1, 1> for Pow<EXP> {
+    fn gate<V: Val>(i: [V; 1]) -> [V; 1] {
+        let [x] = i;
+        let zeros = EXP.leading_zeros() as u8;
+
+        let mut res = x.clone();
+        for i in (zeros + 1)..8 {
+            let bit = (EXP >> (7 - i)) & 0b1;
+            res = res.clone() * res;
+            if bit == 1 {
+                res = res * x.clone();
+            }
+        }
+        [res]
+    }
+
+    fn check<V: Val>(i: [V; 1], o: [V; 1]) -> Constraints<V> {
+        let [x] = i;
+        let zeros = EXP.leading_zeros() as u8;
+
+        let mut res = x.clone();
+        for i in (zeros + 1)..8 {
+            let bit = (EXP >> (7 - i)) & 0b1;
+            res = res.clone() * res;
+            if bit == 1 {
+                res = res * x.clone();
+            }
+        }
+        let [power] = o;
+        Constraints::from(res - power)
+    }
+}
+
+//TODO: this requires constants
+/*
+/// Gate to enforce its only input is 0 or 1.
+pub enum Binary {}
+
+impl Gate<1, 1, 0> for Binary {
+    fn gate<V: Val>(_i: [V; 1]) -> [V; 0] {
+        todo!()
+    }
+
+    fn check<V: Val>(i: [V; 1], o: [V; 0]) -> Constraints<V> {
+        todo!()
+    }
+}
+*/
--- ccs/src/lib.rs
+++ ccs/src/lib.rs
@@ -1,4 +1,5 @@
 pub mod circuit;
 pub mod constraint_system;
+pub mod gates;
 pub mod structure;
 pub mod witness;
